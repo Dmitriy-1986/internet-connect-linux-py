5a1e439c300db07c9bc29b77b108e0cb8a2b434d
Dovgal Dima <52016913+Dmitriy-1986@users.noreply.github.com>
2023-12-22 07:25:05 +0200

    Add files via upload

---
+++ inet.py
@@ -0,0 +1,34 @@
+import requests
+import subprocess
+import time
+
+WIFI_SSID = "Zaporiggia"
+WIFI_PASSWORD = "172334455"
+
+def check_internet():
+    url = "http://www.google.com"
+    try:
+        response = requests.get(url, timeout=5)
+        if response.status_code == 200:
+            return True
+        else:
+            return False
+    except requests.RequestException as e:
+        print(f"Ошибка при проверке интернета: {e}")
+        return False
+
+def connect_to_wifi(ssid, password):
+    try:
+        cmd = ['sudo', 'nmcli', 'device', 'wifi', 'connect', ssid, 'password', password]
+        subprocess.run(cmd, check=True)  # Проверка успешности выполнения команды
+        print("Успешно подключено к Wi-Fi")
+    except subprocess.CalledProcessError as e:
+        print(f"Ошибка при подключении к Wi-Fi: {e}")
+
+while True:
+    if not check_internet():
+        print("Соединение оборвалось, переподключение к Wi-Fi...")
+        connect_to_wifi(WIFI_SSID, WIFI_PASSWORD)
+    else:
+        print("Интернет подключен")
+    time.sleep(10)  # Проверка каждые 10 секунд
